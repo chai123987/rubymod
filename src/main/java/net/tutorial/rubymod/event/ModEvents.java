bf8eaf1de134b0d31f7b374006dc8f91b029fde5
chai123987 <550219034@qq.com>
2026-06-14 18:23:05 +0800

    Update ModEvents.java

--- src/main/java/net/tutorial/rubymod/event/ModEvents.java
+++ src/main/java/net/tutorial/rubymod/event/ModEvents.java
@@ -3,7 +3,7 @@ package net.tutorial.rubymod.event;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraftforge.bus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,17 +11,14 @@ import net.tutorial.rubymod.RubyMod;
 import net.tutorial.rubymod.entity.ModEntities;
 import net.tutorial.rubymod.entity.custom.RubyGolemEntity;
 
-// bus = MOD：这些事件在模组初始化阶段触发
 @Mod.EventBusSubscriber(modid = RubyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
 
-    // 给实体绑定属性（不写会崩溃）
     @SubscribeEvent
     public static void registerAttributes(EntityAttributeCreationEvent event) {
         event.put(ModEntities.RUBY_GOLEM.get(), RubyGolemEntity.createAttributes().build());
     }
 
-    // 注册自然生成的判定规则：在地表、足够亮度的方块上才能刷出
     @SubscribeEvent
     public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
         event.register(ModEntities.RUBY_GOLEM.get(),
