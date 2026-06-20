66485d08737316d9d854ddbfa3f0148efe9c6538
chai123987 <550219034@qq.com>
2026-06-20 16:05:50 +0800

    Update RubySkeletonRenderer.java

--- src/main/java/net/tutorial/rubymod/entity/client/RubySkeletonRenderer.java
+++ src/main/java/net/tutorial/rubymod/entity/client/RubySkeletonRenderer.java
@@ -1,23 +1,30 @@
 package net.tutorial.rubymod.entity.client;
 
+import net.minecraft.client.model.SkeletonModel;
 import net.minecraft.client.model.geom.ModelLayers;
-import net.minecraft.client.renderer.entity.AbstractSkeletonRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
 import net.tutorial.rubymod.RubyMod;
 import net.tutorial.rubymod.entity.custom.RubySkeletonEntity;
 
-public class RubySkeletonRenderer extends AbstractSkeletonRenderer<RubySkeletonEntity> {
-
-    private static final ResourceLocation TEXTURE =
-            new ResourceLocation(RubyMod.MOD_ID, "textures/entity/ruby_skeleton.png");
+public class RubySkeletonRenderer extends HumanoidMobRenderer<RubySkeletonEntity, SkeletonModel<RubySkeletonEntity>> {
 
     public RubySkeletonRenderer(EntityRendererProvider.Context context) {
-        super(context, ModelLayers.SKELETON, ModelLayers.SKELETON_INNER_ARMOR, ModelLayers.SKELETON_OUTER_ARMOR);
+        super(context, new SkeletonModel<>(context.bakeLayer(ModelLayers.SKELETON)), 0.5F);
+
+        this.addLayer(new HumanoidArmorLayer<>(this,
+                new SkeletonModel<>(context.bakeLayer(ModelLayers.SKELETON_INNER_ARMOR)),
+                new SkeletonModel<>(context.bakeLayer(ModelLayers.SKELETON_OUTER_ARMOR)),
+                context.getModelManager()));
+
+        this.addLayer(new ItemInHandLayer<>(this, context.getItemInHandRenderer()));
     }
 
     @Override
     public ResourceLocation getTextureLocation(RubySkeletonEntity entity) {
-        return TEXTURE;
+        return new ResourceLocation(RubyMod.MOD_ID, "textures/entity/ruby_skeleton.png");
     }
 }
