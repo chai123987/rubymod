b155a547132d909e6ae116553e7526549afcc4de
chai123987 <550219034@qq.com>
2026-06-20 16:02:02 +0800

    Update RubySkeletonRenderer.java

--- src/main/java/net/tutorial/rubymod/entity/client/RubySkeletonRenderer.java
+++ src/main/java/net/tutorial/rubymod/entity/client/RubySkeletonRenderer.java
@@ -1,23 +1,23 @@
 package net.tutorial.rubymod.entity.client;
 
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.renderer.entity.AbstractSkeletonRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.SkeletonRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.monster.Skeleton;
 import net.tutorial.rubymod.RubyMod;
+import net.tutorial.rubymod.entity.custom.RubySkeletonEntity;
 
-// 继承原版骷髅渲染器：自带骷髅模型 + 持弓/持剑姿势 + 盔甲层，只换红色贴图
-public class RubySkeletonRenderer extends SkeletonRenderer {
+public class RubySkeletonRenderer extends AbstractSkeletonRenderer<RubySkeletonEntity> {
 
     private static final ResourceLocation TEXTURE =
             new ResourceLocation(RubyMod.MOD_ID, "textures/entity/ruby_skeleton.png");
 
     public RubySkeletonRenderer(EntityRendererProvider.Context context) {
-        super(context);
+        super(context, ModelLayers.SKELETON, ModelLayers.SKELETON_INNER_ARMOR, ModelLayers.SKELETON_OUTER_ARMOR);
     }
 
     @Override
-    public ResourceLocation getTextureLocation(Skeleton entity) {
+    public ResourceLocation getTextureLocation(RubySkeletonEntity entity) {
         return TEXTURE;
     }
 }
