3c8a06ec308993045931ae84bf635c68afbbb09e
chai123987 <550219034@qq.com>
2026-07-04 11:00:30 +0800

    Update RubyEndermanRenderer.java

--- src/main/java/net/tutorial/rubymod/entity/client/RubyEndermanRenderer.java
+++ src/main/java/net/tutorial/rubymod/entity/client/RubyEndermanRenderer.java
@@ -3,10 +3,9 @@ package net.tutorial.rubymod.entity.client;
 import net.minecraft.client.renderer.entity.EndermanRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.EnderMan;
 import net.tutorial.rubymod.RubyMod;
-import net.tutorial.rubymod.entity.custom.RubyEndermanEntity;
 
-// 复用原版末影人模型，只换贴图为红色版
 public class RubyEndermanRenderer extends EndermanRenderer {
 
     private static final ResourceLocation TEXTURE =
@@ -17,7 +16,7 @@ public class RubyEndermanRenderer extends EndermanRenderer {
     }
 
     @Override
-    public ResourceLocation getTextureLocation(RubyEndermanEntity entity) {
+    public ResourceLocation getTextureLocation(EnderMan entity) {
         return TEXTURE;
     }
 }
