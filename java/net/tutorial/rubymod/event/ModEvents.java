2e27801cb008f213c1ac8830021c351b18e2ef10
chai123987 <550219034@qq.com>
2026-07-04 13:13:32 +0800

    Update ModEvents.java

--- java/net/tutorial/rubymod/event/ModEvents.java
+++ java/net/tutorial/rubymod/event/ModEvents.java
@@ -9,34 +9,45 @@ import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.tutorial.rubymod.RubyMod;
 import net.tutorial.rubymod.entity.ModEntities;
-import net.tutorial.rubymod.entity.custom.RubyCreeperEntity;
-import net.tutorial.rubymod.entity.custom.RubySkeletonEntity;
-import net.tutorial.rubymod.entity.custom.RubyGolemEntity;
-import net.tutorial.rubymod.entity.custom.AdamantineRubyGolemEntity;
-import net.tutorial.rubymod.entity.custom.RubySpiderEntity;
-import net.tutorial.rubymod.entity.custom.RubyEvokerEntity;
-import net.tutorial.rubymod.entity.custom.RubyPillagerEntity;
-import net.tutorial.rubymod.entity.custom.RubyPlayerEntity;
+
+import net.tutorial.rubymod.entity.custom.*;
 
 @Mod.EventBusSubscriber(modid = RubyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
 
-   @SubscribeEvent
-    public static void addSpawnEggToVanillaTab(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
-            event.accept(ModItems.BUTTERFLY_SPAWN_EGG.get());
-        }
-    }
-        event.put(ModEntities.ADAMANTINE_RUBY_GOLEM.get(), AdamantineRubyGolemEntity.createAttributes().build());
-        event.put(ModEntities.RUBY_SPIDER.get(), RubySpiderEntity.createAttributes().build());
-        event.put(ModEntities.RUBY_EVOKER.get(), RubyEvokerEntity.createAttributes().build());
-        event.put(ModEntities.RUBY_PILLAGER.get(), RubyPillagerEntity.createAttributes().build());
-        event.put(ModEntities.RUBY_PLAYER.get(), RubyPlayerEntity.createAttributes().build());
+    @SubscribeEvent
+    public static void registerAttributes(EntityAttributeCreationEvent event) {
+
+        event.put(ModEntities.RUBY_GOLEM.get(),
+                RubyGolemEntity.createAttributes().build());
+
+        event.put(ModEntities.RUBY_CREEPER.get(),
+                RubyCreeperEntity.createAttributes().build());
+
+        event.put(ModEntities.RUBY_SKELETON.get(),
+                RubySkeletonEntity.createAttributes().build());
+
+        event.put(ModEntities.ADAMANTINE_RUBY_GOLEM.get(),
+                AdamantineRubyGolemEntity.createAttributes().build());
+
+        event.put(ModEntities.RUBY_SPIDER.get(),
+                RubySpiderEntity.createAttributes().build());
+
+        event.put(ModEntities.RUBY_EVOKER.get(),
+                RubyEvokerEntity.createAttributes().build());
+
+        event.put(ModEntities.RUBY_PILLAGER.get(),
+                RubyPillagerEntity.createAttributes().build());
+
+        event.put(ModEntities.RUBY_PLAYER.get(),
+                RubyPlayerEntity.createAttributes().build());
+
+        // ❌ Butterfly 已彻底移除
     }
 
     @SubscribeEvent
     public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
-        // 怪物生成规则：在黑暗的地表生成
+
         event.register(ModEntities.RUBY_GOLEM.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
@@ -72,6 +83,7 @@ public class ModEvents {
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
-        // 注意：红宝石玩家是BOSS，只能用刷怪蛋放，不注册自然生成
+
+        // BOSS：RUBY_PLAYER 不自然生成
     }
 }
