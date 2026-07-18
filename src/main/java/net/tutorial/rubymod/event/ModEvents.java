acf14c2ac7280cbd500da1eeab8243f0b57073d2
chai123987 <550219034@qq.com>
2026-07-18 09:51:47 +0800

    Update ModEvents.java

--- src/main/java/net/tutorial/rubymod/event/ModEvents.java
+++ src/main/java/net/tutorial/rubymod/event/ModEvents.java
@@ -7,20 +7,10 @@ import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
+import net.minecraftforge.registries.RegistryObject;
 import net.tutorial.rubymod.RubyMod;
 import net.tutorial.rubymod.entity.ModEntities;
-
-import net.tutorial.rubymod.entity.custom.RubyGolemEntity;
-import net.tutorial.rubymod.entity.custom.RubyCreeperEntity;
-import net.tutorial.rubymod.entity.custom.RubySkeletonEntity;
-import net.tutorial.rubymod.entity.custom.AdamantineRubyGolemEntity;
-import net.tutorial.rubymod.entity.custom.RubySpiderEntity;
-import net.tutorial.rubymod.entity.custom.RubyEvokerEntity;
-import net.tutorial.rubymod.entity.custom.RubyPillagerEntity;
-import net.tutorial.rubymod.entity.custom.RubyPlayerEntity;
-import net.tutorial.rubymod.entity.custom.RubyEndermanEntity;
-
+import net.tutorial.rubymod.entity.custom.*;
 
 @Mod.EventBusSubscriber(modid = RubyMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEvents {
@@ -63,54 +53,51 @@ public class ModEvents {
     public static void registerSpawnPlacements(SpawnPlacementRegisterEvent event) {
 
 
-        event.register(ModEntities.RUBY_GOLEM.get(),
+        event.register(
+                ModEntities.RUBY_GOLEM.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
 
 
-        event.register(ModEntities.RUBY_CREEPER.get(),
+        event.register(
+                ModEntities.RUBY_CREEPER.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
 
 
-        event.register(ModEntities.RUBY_SKELETON.get(),
+        event.register(
+                ModEntities.RUBY_SKELETON.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
 
 
-        event.register(ModEntities.RUBY_SPIDER.get(),
+        event.register(
+                ModEntities.RUBY_SPIDER.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
 
 
-        event.register(ModEntities.RUBY_EVOKER.get(),
+        event.register(
+                ModEntities.RUBY_EVOKER.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
 
 
-        event.register(ModEntities.RUBY_PILLAGER.get(),
+        event.register(
+                ModEntities.RUBY_PILLAGER.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
                 Monster::checkMonsterSpawnRules,
                 SpawnPlacementRegisterEvent.Operation.AND);
-
-
-        event.register(ModEntities.RUBY_ENDERMAN.get(),
-                SpawnPlacements.Type.ON_GROUND,
-                Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-                Monster::checkMonsterSpawnRules,
-                SpawnPlacementRegisterEvent.Operation.AND);
-
     }
-
 }
